e5dff3980e6d3b6422956177d6c86f9ce0421ea5
sachinm78 <sachinm78@gmail.com>
2019-10-16 12:06:44 -0500

    edited anagrams, solved capitalize

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -21,15 +21,6 @@ function anagrams(stringA, stringB) {
       .join('');
   }
 
-function buildCharMap(str) {
-  const charMap = {};
-
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-
-  return charMap;
-}
 
 module.exports = anagrams;
 
