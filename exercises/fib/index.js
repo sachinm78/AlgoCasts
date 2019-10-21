72ff4c6cd42baed9093669fd362550af18a4d4d1
sachinm78 <sachinm78@gmail.com>
2019-10-21 16:26:39 -0500

    solved fib, queue, stack, weave, and qfroms

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,11 +8,50 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    if(n <2) {
-        return n;
+// memoized function => vastly improves runtime
+function memoize(fn) {
+    const cache = {}
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args]
+        }
+
+        const result = fn.apply(this, args)
+        cache[args] = result
+
+        return result
     }
-    return fib(n-1) + fib(n+2);
 }
 
+function slowFib(n) {
+    if (n<2) {
+        return n;
+    }
+    return fib(n-1) + fib(n-2)
+}
+
+const fib = memoize(slowFib)
+
 module.exports = fib;
+
+// iterative solution => linear runtime 
+// function fib(n) {
+//     const result = [0, 1]
+
+//     for (let i=2; i<=n; i++) {
+//         const a = result[i-1]
+//         const b = result[i-2]
+
+//         result.push(a+b)
+//     }
+
+//     return result[n]
+// }
+
+// recursive solution => exponential runtime
+// function fib(n) {
+//     if (n<2) {
+//         return n;
+//     }
+//     return fib(n-1) + fib(n-2)
+// }
